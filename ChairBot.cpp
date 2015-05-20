620876aa01744c8fcf5b5b36a47226690eea8c8c
Steve Bowtie <steven.bowtie@gmail.com>
2015-05-19 22:04:24 -0700

    Working, tested version. Serial communicates properly. Time to add sonar.

--- ChairBot.cpp
+++ ChairBot.cpp
@@ -3,21 +3,21 @@
 #include <NewPing.h>
 #include "ChairBot.h"
 
-int MAX_SPEED=128;
-
+int MAX_SPEED=1028;
 SoftwareSerial motor(2,3);
-NewPing left_sonar(7,8);
+//NewPing left_sonar(7,8);
 
 ChairBot::ChairBot(){
-	attachInterrupt(0, ChairBot::count_pulse, CHANGE);
+//	attachInterrupt(0, ChairBot::count_pulse, CHANGE);
+	motor.begin(9600);
 	}
 
 void ChairBot::drive(int left, int right){
-	left=max( -MAX_SPEED, min( MAX_SPEED, left) );
-	right=max( -MAX_SPEED, min( MAX_SPEED, right) );
-	motor.print("M0:");
-	motor.println(left);
+	//left=max( -MAX_SPEED, min( MAX_SPEED, left) );
+	//right=max( -MAX_SPEED, min( MAX_SPEED, right) );
 	motor.print("M1:");
+	motor.println(left);
+	motor.print("M2:");
 	motor.println(right);
 	}
 
@@ -25,6 +25,7 @@ void ChairBot::check_sonar(){
 	
 	}
 
+/*
 void ChairBot::count_pulse(){
 	if (pulse_toggle){
 		pulse_stop=micros();
@@ -33,4 +34,4 @@ void ChairBot::count_pulse(){
 		pulse_start=micros();
 		}
 	}
-
+*/
--- example/ChairBot_Basic/ChairBot_Basic.ino
+++ example/ChairBot_Basic/ChairBot_Basic.ino
@@ -5,10 +5,19 @@
 ChairBot cb;
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
-	cb.drive(128,128); 
-
+  Serial.println(analogRead(0));
+  delay(100);
+  /*
+  cb.drive(2000, 2000); 
+  delay(500);
+  cb.drive(-2000, 2000);
+  delay(500); 
+  cb.drive(2000, -2000);
+  delay(500); 
+  */
 }
